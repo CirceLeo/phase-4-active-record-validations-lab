a4239d275e194c12275dc032c5fe37d7ef23a9ba
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-26 13:46:56 -0400

    commit tree 5c86a2be33edbd78a81224d31cb73f27f41f6047

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :click_bait
+
+    def click_bait
+        if title
+            if !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Guess") && !title.include?("Top")
+                errors.add(:title, "isn't click bait")
+            end
+            
+        end
+    end
+
 end
